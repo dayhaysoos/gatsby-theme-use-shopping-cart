7297c6fdfe822cc2d180ac8a9134d242e338b0aa
Nick DeJesus <ndejesus1227@gmail.com>
2020-11-24 16:39:07 -0500

    add gatsby prefix

--- packages/gatsby-theme-use-shopping-cart/gatsby-browser.js
+++ packages/gatsby-theme-use-shopping-cart/gatsby-browser.js
@@ -2,7 +2,7 @@ const React = require('react')
 const { CartProvider } = require('use-shopping-cart')
 const { loadStripe } = require('@stripe/stripe-js')
 
-const stripePromise = loadStripe(process.env.STRIPE_API_PUBLIC)
+const stripePromise = loadStripe(process.env.GATSBY_STRIPE_API_PUBLIC)
 
 export const wrapRootElement = (
   { element },
